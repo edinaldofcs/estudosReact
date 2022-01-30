631d28f8fec4fe25aad3030faecbd54fa0cbbf1f
Edinaldo Fernandes Cardoso Santos <fernandescro5@gmail.com>
2022-01-30 14:24:22 -0300

    comitando aula2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 function App() {
+
+  function soma(a,b){
+    return a + b
+  }
+
   return (
     <div className="App">
-      primeiro app
+      <h1>Soma</h1>
+      <h2>A soma de {5} e {7} = {soma(5, 7)}</h2>
     </div>
   );
 }
